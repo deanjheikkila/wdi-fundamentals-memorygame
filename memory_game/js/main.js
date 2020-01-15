eec112144f48bf654f2e060c17f954477114b18b
Dean <dean.overflow@gmail.com>
2020-01-14 23:16:33 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,20 +18,21 @@ const cards = [
     {
         rank:"king",
         suit: "diamonds",
-        cardImage: "images/king-of-hearts.png"
+        cardImage: "images/king-of-diamonds.png"
     }
 ];
 const cardsInPlay = [];
 
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
       } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
       }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+    cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage);
@@ -39,8 +40,17 @@ function flipCard(cardId) {
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
+    this.setAttribute('src', cards[cardId].cardImage);
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+    for (let i = 0; i < cards.length; i++){
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
 
+createBoard();
